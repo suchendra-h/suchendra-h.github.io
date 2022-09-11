b0e3932b021c45b40d153ece9b87f88666d7e5b1
Milad Hassani <suchendrahassani@gmail.com>
2022-09-11 04:32:45 -0400

    added the country map layer over the visualization

--- src/components/pages/MusicGlobe.jsx
+++ src/components/pages/MusicGlobe.jsx
@@ -1,14 +1,51 @@
 import Globe from "react-globe.gl";
-import { gData } from "../utils/globeUtils";
+import * as d3 from "d3";
+import { useState, useMemo, useEffect } from "react";
+import { country_data } from "../utils/data/coutries.js";
 
 function MusicGlobe() {
+  const [countries, setCountries] = useState({ features: [] });
+  const [hoverD, setHoverD] = useState();
+
+  useEffect(() => {
+    // load data
+    setCountries(country_data);
+  }, []);
+
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+
+  // GDP per capita (avoiding countries with small pop)
+  const getVal = (feat) =>
+    feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+
+  const maxVal = useMemo(
+    () => Math.max(...countries.features.map(getVal)),
+    [countries]
+  );
+  colorScale.domain([0, maxVal]);
   return (
     <>
+      {console.log(countries)}
       <Globe
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        pointsData={gData}
-        pointAltitude="size"
-        pointColor="color"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        lineHoverPrecision={0}
+        polygonsData={countries.features.filter(
+          (d) => d.properties.ISO_A2 !== "AQ"
+        )}
+        polygonAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
+        polygonCapColor={(d) =>
+          d === hoverD ? "steelblue" : colorScale(getVal(d))
+        }
+        polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+        polygonStrokeColor={() => "#111"}
+        polygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+        GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+        Population: <i>${d.POP_EST}</i>
+      `}
+        onPolygonHover={setHoverD}
+        polygonsTransitionDuration={300}
       />
     </>
   );
