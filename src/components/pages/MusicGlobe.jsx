312cd1005f8d4fca75d5ab156f636d6cb72ac21e
Milad Hassani <suchendrahassani@gmail.com>
2022-09-11 18:55:34 -0400

    added the songs information table. Data from lastfm, needs styling.

--- src/components/pages/MusicGlobe.jsx
+++ src/components/pages/MusicGlobe.jsx
@@ -10,6 +10,13 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import { typography } from "@mui/system";
 
 function PolygonLabel({ properties: d }) {
@@ -19,10 +26,16 @@ function PolygonLabel({ properties: d }) {
   return ReactDOMServer.renderToStaticMarkup(lbl);
 }
 
+function SongsTable(props) {
+  if (props.data) {
+  }
+}
+
 function MusicGlobe() {
   const [countries, setCountries] = useState({ features: [] });
   const [hoverD, setHoverD] = useState();
   const [clickD, setClickD] = useState();
+  const [songsData, setSongsData] = useState();
 
   useEffect(() => {
     // load data
@@ -40,14 +53,82 @@ function MusicGlobe() {
     [countries]
   );
   colorScale.domain([0, maxVal]);
+  // what happens when a country is clicked
+  function handleClick(d) {
+    setClickD(d);
+    let aKey = process.env.REACT_APP_LASTFM_API_KEY;
+    let url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${d.properties.BRK_NAME}&api_key=${aKey}&format=json&limit=10`;
+
+    // fetch 10 most popular songs for the country.
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.message ? setSongsData(null) : setSongsData(data.tracks.track);
+      });
+    console.log(songsData);
+  }
   // ===================
   // music card contents
   // ===================
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
   const card = (
     <React.Fragment>
       <CardContent>
         {clickD ? (
-          <Typography variant="h5">{clickD.properties.ADMIN} </Typography>
+          <>
+            <Typography variant="h5">{clickD.properties.ADMIN} </Typography>
+            {songsData ? (
+              <TableContainer component={Paper}>
+                <Table
+                  sx={{ minWidth: 650 }}
+                  size="small"
+                  aria-label="a dense table"
+                >
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Song Name</TableCell>
+                      <TableCell align="right">Artist</TableCell>
+                      <TableCell align="right">Listeners</TableCell>
+                      <TableCell align="right">Link</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {songsData.map((row) => (
+                      <TableRow
+                        key={row.name}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.artist.name}</TableCell>
+                        <TableCell align="right">{row.listeners}</TableCell>
+                        <TableCell align="right">
+                          <a href={`${row.url}`}>{row.url}</a>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            ) : (
+              <Typography variant="h5">
+                {"Could not find the songs' information for this country"}
+              </Typography>
+            )}
+          </>
         ) : (
           <Typography variant="h5">
             {"I will show trending music in every country, just select one!"}
@@ -83,7 +164,7 @@ function MusicGlobe() {
         polygonStrokeColor={() => "#111"}
         polygonLabel={PolygonLabel}
         onPolygonHover={setHoverD}
-        onPolygonClick={setClickD}
+        onPolygonClick={handleClick}
         polygonsTransitionDuration={300}
       />
     </>
